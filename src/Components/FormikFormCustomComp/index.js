7f4b2c768c55b13e9fa2a47f00ca19baefe4a270
Federico <depetrisfe@gmail.com>
2021-10-16 00:51:47 -0300

    new components added

--- src/Components/FormikFormCustomComp/index.js
+++ src/Components/FormikFormCustomComp/index.js
@@ -1,5 +1,8 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
+import CheckboxInput from './CustomComponents/CheckboxInput';
 import TextInput from './CustomComponents/TextInput';
+import SelectInput from './CustomComponents/SelectInput';
+import RadioInput from './CustomComponents/RadioInput';
 import './styles.css';
 
 const validate = (values) => {
@@ -15,6 +18,10 @@ const validate = (values) => {
     }else if (values.lastname.length < 5){
         errors.lastname = 'Apellido muy corto'
     }
+
+    if(!values.radio){
+        errors.radio = 'Requerido'
+    }
     //propiedad del objeto que debe tener el mismo nombre que el campo que valido
     //ejemplo name = errors.name, lastname = errors.lastname
     return errors
@@ -24,7 +31,7 @@ const FormikFormCustomComp = () => {
 
     return(
         <Formik
-            initialValues={{name: '', lastname: '', email: '',}}
+            initialValues={{name: '', lastname: '', email: '', options: '', radio: ''}}
             validate={validate}
             onSubmit={values => console.log(values)}
         >
@@ -36,6 +43,23 @@ const FormikFormCustomComp = () => {
                         <TextInput name='name' label='Nombre' autoComplete='off'/>
                         <TextInput name='lastname' label='Apellido' autoComplete='off'/>
                         <TextInput name='email' label='Email' autoComplete='off'/>
+                        <br/>
+                        <SelectInput label="opciones" name="options">
+                            <option value="option1">opcion 1</option>
+                            <option value="option2">opcion 2</option>
+                            <option value="option3">opcion 3</option>
+                        </SelectInput>
+                        <br/>
+                        <CheckboxInput name='accept'>
+                            Aceptar terminos y condiciones
+                        </CheckboxInput>
+                        <br/>
+                        <div className='radioGroup'>
+                            <RadioInput name="radio" value="radio1" label="radio 1"/>
+                            <RadioInput name="radio" value="radio2" label="radio 2"/>
+                            <RadioInput name="radio" value="radio3" label="radio 3"/>
+                        </div>
+                        <ErrorMessage name="radio"/>
                         <button
                             type="submit"
                             className='acceptFormikButton'
